44de06cb685a1699341ef9e7c50c28bbc6c671e4
Andrew Fraser Soñett <andrew8805@gmail.com>
2022-02-23 14:49:29 -0500

    update index.js

--- index.js
+++ index.js
@@ -48,7 +48,9 @@ function verifySignatureCololmbiaRed(body, signature) {
 async function sendDataToBitrix24(req,res, secretKey) {
     if (!verifySignature(req.body, req.headers["x-tawk-signature"],secretKey)) {
         console.log("verification failed");
-        res.send("verification failed");
+        //res.send("verification failed");
+    }else{
+        console.log("verification OK!");
     }
 
     // how to use Bitrix24 inbound webhook: https://training.bitrix24.com/rest_help/rest_sum/webhooks.php
@@ -128,112 +130,6 @@ async function sendDataToBitrix24(req,res, secretKey) {
             res.send("no valid option sent");
     }
 }
-
-/*
-app.post("/webhookssss", async (req, res, next) => {
-    try {
-        if (!verifySignature(req.body, req.headers["x-tawk-signature"])) {
-            res.send("verification failed");
-        }
-
-        // how to use Bitrix24 inbound webhook: https://training.bitrix24.com/rest_help/rest_sum/webhooks.php
-        const eventID = req.header("X-Hook-Event-Id");
-        switch (eventID) {
-            case "chat:start":
-                // const queryUrl = 'https://restapi.bitrix24.com/rest/1/31uhq2q855fk1foj/crm.lead.add.json';
-                // $queryData = http_build_query(array(
-                //     'fields' => array(
-                //     "TITLE" => $_REQUEST['first_name'].' '.$_REQUEST['last_name'],
-                //     "NAME" => $_REQUEST['first_name'],
-                //     "LAST_NAME" => $_REQUEST['last_name'],
-                //     "STATUS_ID" => "NEW",
-                //     "OPENED" => "Y",
-                //     "ASSIGNED_BY_ID" => 1,
-                //     "PHONE" => array(array("VALUE" => $_REQUEST['phone'], "VALUE_TYPE" => "WORK" )),
-                //     "EMAIL" => array(array("VALUE" => $_REQUEST['email'], "VALUE_TYPE" => "WORK" )),
-                //     ),
-                //     'params' => array("REGISTER_SONET_EVENT" => "Y")
-                //     ));
-
-                // get data from the tawk.to request
-                const body = {
-                    fields: {
-                        TITLE: "Saily Prueba",
-                        UF_CRM_1625751580135: ["176"],
-                        UF_CRM_1626274801587: ["192"],
-                        UF_CRM_1626357192293: ["204"],
-                        UF_CRM_1638810416867: "",
-                        OPENED: "Y",
-                        ASSIGNED_BY_ID: 1,
-                        CREATED_BY_ID: 1,
-                        PHONE: "1234567890",
-                        EMAIL: "sailyvaro05@gmail.com",
-                    },
-                    params: {
-                        REGISTER_SONET_EVENT: "Y",
-                    },
-                };
-
-                const result = await axios.post(
-                    "https://restapi.bitrix24.com/rest/1/31uhq2q855fk1foj/crm.lead.add.json",
-                    body
-                );
-                res.send(result);
-
-                break;
-            case "chat:end":
-                body = {
-                    fields: {
-                        TITLE: "Saily Prueba",
-                        UF_CRM_1625751580135: ["176"],
-                        UF_CRM_1626274801587: ["192"],
-                        UF_CRM_1626357192293: ["204"],
-                        UF_CRM_1638810416867: "",
-                        OPENED: "Y",
-                        ASSIGNED_BY_ID: 1,
-                        CREATED_BY_ID: 1,
-                        PHONE: "1234567890",
-                        EMAIL: "sailyvaro05@gmail.com",
-                    },
-                    params: {
-                        REGISTER_SONET_EVENT: "Y",
-                    },
-                };
-                res.send("chat end");
-                break;
-            case "ticket:create":
-                body = {
-                    fields: {
-                        TITLE: "Saily Prueba",
-                        UF_CRM_1625751580135: ["176"],
-                        UF_CRM_1626274801587: ["192"],
-                        UF_CRM_1626357192293: ["204"],
-                        UF_CRM_1638810416867: "",
-                        OPENED: "Y",
-                        ASSIGNED_BY_ID: 1,
-                        CREATED_BY_ID: 1,
-                        PHONE: "1234567890",
-                        EMAIL: "sailyvaro05@gmail.com",
-                    },
-                    params: {
-                        REGISTER_SONET_EVENT: "Y",
-                    },
-                };
-                res.send("ticket create");
-                break;
-            default:
-                res.send("no valid option sent");
-        }
-    } catch (error) {
-        console.error(error.message);
-        return res.status(500).json({
-            message: error.message,
-        });
-        //res.send(error.message);
-    }
-
-});
-*/
 
 app.post("/colombiared", async (req, res) => {
     try {
